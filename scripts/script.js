ca7da262c4995d7de470f627d92d6f6c33bcb478
Rajveer Singh <singhrajveer4021@gmail.com>
2022-11-09 13:30:25 -0800

    practiced do while loop and alert user_input

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,24 @@
-for (let i = 1; i < 11; i++) {
-    console.log(2**i)
-}
+// for (let i = 1; i < 11; i++) {
+//     console.log(2**i)
+// }
 
-let i = 1;
-while (i < 11) {
-    console.log(Math.pow(2, i)); //2**i
-    i++;
-}
+// let i = 1;
+// while (i < 11) {
+//     console.log(Math.pow(2, i)); //2**i
+//     i++;
+// }
+
+let userInput;
+let numberedInput;
+do{
+    userInput = prompt("what is your name?");
+    numberedInput = Number(userInput)
+    console.log(Number (userInput));
+} while (!isNaN(numberedInput));
+
+alert(`Hello ${userInput}`)
+
+
 
 
 
